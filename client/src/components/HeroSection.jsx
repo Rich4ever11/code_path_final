50ac3742b61165f41f646519a41685572cefdc89
Rich4ever11 <rich4ever11@protonmail.com>
2024-11-01 18:50:25 -0400

    improved styling

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -14,15 +14,16 @@ export default function HeroSection() {
             <div className="divider"></div>
 
             <p className="text-white py-8 font-thin">
-              This location will take you to a place that connects you with like
-              minded travels and engage with amazing other bloggers 🥂
+              Where the offline meets realife. Let the location intrigue and
+              guide you to a connection that will foster for many days and
+              nights. 🥂
             </p>
 
             <div className="flex justify-center">
               <Button
                 variant="bordered"
                 size="lg"
-                className="bg-gradient-to-tr from-orange-200/50 to-blue-950/10 border-2 border-white text-white shadow-lg text-4xl p-8 font-thin"
+                className="bg-gradient-to-tr from-orange-200/50 to-blue-950/10 border-2 border-white text-white shadow-lg text-4xl p-8 font-thin rounded-none"
               >
                 Login
               </Button>
@@ -30,7 +31,7 @@ export default function HeroSection() {
               <Button
                 color="primary"
                 variant="bordered"
-                className="bg-gradient-to-tr from-orange-200/50 to-blue-950/10 border-2 border-white text-white shadow-lg text-4xl p-8 font-thin"
+                className="bg-gradient-to-tr from-orange-200/50 to-blue-950/10 border-2 border-white text-white shadow-lg text-4xl p-8 font-thin rounded-none"
               >
                 Register
               </Button>
@@ -38,7 +39,10 @@ export default function HeroSection() {
           </div>
         </div>
       </div>
-      <div className="bg-gradient-to-b from-black/50 h-screen w-full absolute"></div>
+      <div
+        className="bg-gradient-to-t from-black/25 h-screen w-full absolute"
+        style={{ pointerEvents: "none" }}
+      ></div>
       <img
         className="object-cover bg-gradient-to-r from-indigo-50 h-screen w-full"
         src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
